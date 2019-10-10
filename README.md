1ba11f3f5d9cfa3d0ffa0e5dca26e5fc6f334b84
Aleksbgbg <kozichka01@abv.bg>
2019-10-10 21:39:55 +0100

    Initialise repository.

---
+++ README.md
@@ -0,0 +1,2 @@
+# Nexus
+A webpage that has connections to all of my web applications.
---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+main
+  router-view
+</template>
+
+<style lang="stylus">
+html, body
+  margin: 0
+  padding: 0
+
+#app
+  font-family 'Avenir', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing antialiased
+  -moz-osx-font-smoothing grayscale
+</style>
