57fb80e8337f520666bde7432fa81cb8e6a522ca
Aleksbgbg <kozichka01@abv.bg>
2019-11-24 11:52:21 +0000

    [websites.ts] Add favicon and documentation for API.

--- src/websites.ts
+++ src/websites.ts
@@ -26,5 +26,5 @@ export const websites: Website[] = [
   new WebsiteImpl("Nexus", "nexus", "/favicon.png", "Has connections to all of my web applications. The page you're looking at right now."),
   new WebsiteImpl("Radio", "radio", "/icon/radio.png", "Allows multiple users in the same radio channel to listen to the same music and chat. Users can create their own radio channels from YouTube playlists. Radio channels rotate and shuffle their playlists 24/7."),
   new WebsiteImpl("Downloads", "downloads", "/favicon.png", "Contains download links to all of my released desktop applications."),
-  new WebsiteImpl("API", "api", "", "API server for some of my desktop applications. Root page will soon be documented so others can use my endpoints to extend or replace desktop clients.")
+  new WebsiteImpl("API", "api", "/favicon.png", "API server for some of my desktop applications. The root page is documented so others can use my endpoints to extend or replace desktop clients.")
 ];
